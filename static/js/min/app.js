e390eeec7f5a3b816c18a07c145decfd001363ae
Fabio Rodriguez <realgt@gmail.com>
2011-04-16 12:32:54 -0400

    minified js

--- static/js/min/app.js
+++ static/js/min/app.js
@@ -1,1 +1,38 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('9 H=A;14.1T=7(){1U("V()",1W)}9 b=29 1h.1w(1q,{23:1t,1u:A});b.q();b.x(\'f\',7(5){8(5.c){12(5.c)}d 8(5.e){R(5.e);9 v=5.e.v.n();9 L=5.e.L.n();9 J=5.e.J.n();9 F=5.e.F.n();9 G=5.e.G.n();9 N=v+"|"+L+"|"+J+"|"+F+"|"+G;h().1s(N)}d 8(5.y){1r(5.y)}d 8(5.B){z(5.B)}d 8(5.Q){h().1p(5.Q)}d 8(5.f){8(5.f==\'o\'){o();I()}d 8(5.f.1v(0,6)==\'1B\'){M(5.f)}d 8(5.f==\'11\'){z(\'1A, 1z 1o x 1y 11\');C()}}});b.x(\'1m\',7(){Y.10(\'X...\')b.q()});b.x(\'1e\',7(){Y.10(\'1c 1b. X...\')b.q()});7 1n(T){b.j({1l:T})}7 o(){h().o()}7 M(f){h().M(f)}7 I(){8(!b.U){1a("I()",P)}d{b.j({y:W})}}7 1i(){b.j({y:A})}7 1E(l,k,m){b.j({24:{l:l,k:k,m:m}})}7 V(){8(!b.U){o();b.q()}}7 20(){H=W}7 C(){h().C()}7 z(Z){$("#2d").w(\'<S>\'+Z+\'</S>\');$("#B").27(\'28\')}7 R(e){$("#e").w(\'1X: 4,1K,1L\'+e.v.n())}7 12(c){$("#c").w("");8(c[0].p!=O)$("#1G").1H();1N(i=0;i<=c.1O-1;i++){8(c[i].p!=O){9 g=c[i].p.1V("|");9 p=g[0];9 l="1S";9 m="/1P/1Q.1R";9 k="#";8(g[1])l=g[1];8(g[2])m=g[2];8(g[3])k=g[3];9 K=16.1F(\'1I\');9 u=1J.1M(c[i].r/19);9 D=c[i].r-(u*19);9 r=u+" u "+D+" D";$(K).w(\'<a 2a="\'+p+\'" 2b="\'+k+\'"><2c 26="25" 1Z="1Y" 21="22"  E="2e" 1x="\'+m+\'"></a><s E="1j">\'+l+\'</s><s E="1k">\'+r+\'</s>\');$("#c").1g(K)}}}7 13(t){b.j({13:t})}7 15(t){b.j({15:t})}7 h(){8(H)8(1d.1f.1D("1C")!=-1){17 14[\'18\']}d{17 16[\'18\']}d{1a("h()",P)}}',62,139,'|||||data||function|if|var||socket|leaders|else|stats|message|leaderData|getMovie||send|url|name|image|toString|stopStream|id|connect|ts|div|streamNum|min|viewers|html|on|queue|writeAnnouncement|false|announcement|getEntry|sec|class|flag1|flag2|isFlashReady|addToQueue|vote2|li1|vote1|startStream|statsString|undefined|1000|winner|writeStats|em|streamId|connected|checkStream|true|reconnecting|console|msg|log|leaderboard|writeLeaders|vote|window|flag|document|return|Webcam|60|setTimeout|failed|connection|navigator|connect_failed|appName|append|io|removeFromQueue|leaderName|leaderTime|streaming|disconnect|startedStreaming|are|setWinner|null|writeQueue|setGameStats|80|rememberTransport|substr|Socket|src|the|you|Congrats|stream|Microsoft|indexOf|sendEntryData|createElement|leaderBoard|show|li|Math|451|78|floor|for|length|images|unknown|jpg|Anonymous|onload|setInterval|split|30000|Viewers|128|width|asReady|height|90|port|entry|_blank|target|fadeIn|slow|new|alt|href|img|announceText|leaderImage'.split('|'),0,{}))
+var isFlashReady=false;window.onload=function(){socket.connect();setInterval("checkStream()",30000);}
+var socket=new io.Socket(null,{port:80,rememberTransport:false});socket.on('message',function(data){if(data.leaders){writeLeaders(data.leaders);}
+else if(data.gamestats){writeStats(data.gamestats);}
+else if(data.queue){writeQueue(data.queue);}
+else if(data.announcement){writeAnnouncement(data.announcement);}
+else if(data.winner){getMovie().setWinner(data.winner);}
+else if(data.message){if(data.message=='stopStream'){stopStream();addToQueue();}
+else if(data.message.substr(0,6)=='stream'){startStream(data.message);}
+else if(data.message=='leaderboard'){writeAnnouncement('Congrats, you are on the leaderboard');getEntry();}}});socket.on('disconnect',function(){console.log('reconnecting...')
+socket.connect()});socket.on('connect_failed',function(){console.log('connection failed. reconnecting...')
+socket.connect()});function startedStreaming(streamId){socket.send({streaming:streamId});}
+function stopStream(){getMovie().stopStream();}
+function startStream(message){getMovie().startStream(message);}
+function addToQueue(){if(!socket.connected){setTimeout("addToQueue()",1000);}
+else
+{socket.send({queue:true});}}
+function removeFromQueue(){socket.send({queue:false});}
+function sendEntryData(name,url,image){socket.send({entry:{name:name,url:url,image:image}});}
+function checkStream(){if(!socket.connected){stopStream();socket.connect();}}
+function asReady(){isFlashReady=true;}
+function getEntry(){getMovie().getEntry();}
+function writeAnnouncement(msg){$("#announceText").html('<em>'+msg+'</em>');$("#announcement").fadeIn('slow');}
+function writeStats(stats){getMovie().updateStats(stats);var viewers=stats.split("|")[0];$("#stats").html('Viewers: 4,451,78'+viewers);}
+function writeLeaders(leaders){$("#leaders").html("");if(leaders[0].id!=undefined)
+$("#leaderBoard").show();for(i=0;i<=leaders.length-1;i++){if(leaders[i].id!=undefined){var leaderData=leaders[i].id.split("|");var id=leaderData[0];var name="Anonymous";var image="/images/unknown.jpg";var url="#";if(leaderData[1])
+name=leaderData[1];if(leaderData[2])
+image=leaderData[2];if(leaderData[3])
+url=leaderData[3];var li1=document.createElement('li');var min=Math.floor(leaders[i].ts/60);var sec=leaders[i].ts-(min*60);var ts=min+" min "+sec+" sec";$(li1).html('<a alt="'+id+'" href="'+url+'"><img target="_blank" width="128" height="90"  class="leaderImage" src="'+image+'"></a><div class="leaderName">'+name
++'</div><div class="leaderTime">'+ts+'</div>');$("#leaders").append(li1);}}}
+function vote(streamNum){socket.send({vote:streamNum});}
+function flag(streamNum){socket.send({flag:streamNum});}
+function getMovie(){var movieName='Webcam';if(isFlashReady){try
+{movie=document[movieName];movie=(movie==null||movie==undefined)?window[movieName]:movie;}
+catch(e)
+{return null;}
+return movie;}
+else
+{}}
